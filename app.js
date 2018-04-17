b8edbf7649cfdc7ff0e63cd031e9d45349e0fd7c
shady-abdelhamid <shady.abdelhamid@outlook.com>
2018-04-17 10:34:40 +0200

    add db connection setup.

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// DB setup
+//Set up mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/localLiberary';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
